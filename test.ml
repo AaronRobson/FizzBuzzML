84a4899762fea52428584a7a66f0e9320e020019
Aaron Robson <AaronRobson@users.noreply.github.com>
2018-12-17 23:58:45 +0000

    Categorise test code as OCaml
    It was considered StandardML by GitHub.

--- test.ml
+++ test.ml
@@ -1,26 +1,29 @@
 open OUnit
 
 let suite =
-  "FizzBuzz Test Suite" >::: [
-    "Nominal Case" >:: (fun _ ->
-      let result = Library.fizzbuzz 1 in
-      assert_equal result "1"
-    );
+"FizzBuzz Test Suite" >:::
+[
+  "Nominal Case" >:: (fun _ ->
+    let result = Library.fizzbuzz 1 in
+    assert_equal result "1"
+  );
 
-    "Fizz Case" >:: (fun _ ->
-      let result = Library.fizzbuzz 3 in
-      assert_equal result "Fizz"
-    );
+  "Fizz Case" >:: (fun _ ->
+    let result = Library.fizzbuzz 3 in
+    assert_equal result "Fizz"
+  );
 
-    "Buzz Case" >:: (fun _ ->
-      let result = Library.fizzbuzz 5 in
-      assert_equal result "Buzz"
-    );
+  "Buzz Case" >:: (fun _ ->
+    let result = Library.fizzbuzz 5 in
+    assert_equal result "Buzz"
+  );
 
-    "FizzBuzz Case" >:: (fun _ ->
-      let result = Library.fizzbuzz 15 in
-      assert_equal result "FizzBuzz"
-    );
-  ]
+  "FizzBuzz Case" >:: (fun _ ->
+    let result = Library.fizzbuzz 15 in
+    assert_equal result "FizzBuzz"
+  )]
+;;
 
-let _ = run_test_tt_main suite
+let _ =
+  run_test_tt_main suite
+;;
